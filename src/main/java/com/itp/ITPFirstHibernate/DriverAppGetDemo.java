fbda6435640191e53fa9aa160185fe0a9669d9a4
Umar Solanki <uysolanki@gmail.com>
2025-01-31 09:18:33 +0530

    hibernate

---
+++ src/main/java/com/itp/ITPFirstHibernate/DriverAppGetDemo.java
@@ -0,0 +1,34 @@
+package com.itp.ITPFirstHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.itp.ITPFirstHibernate.entity.Employee;
+
+public class DriverAppGetDemo {
+
+	public static void main(String[] args) {
+		Configuration cfg=new Configuration();
+    	cfg.configure("hibernate.cfg.xml");
+    	SessionFactory factory=cfg.buildSessionFactory();
+ 
+    	Session session1=factory.openSession();
+    	Transaction tranx=session1.beginTransaction();
+    	
+    	Employee emp1=session1.get(Employee.class,1); //not found in session so query the db
+    	System.out.println(emp1.getEname());
+    	
+    	Employee emp2=session1.get(Employee.class,2); //not found in session so query the db
+    	System.out.println(emp2.getEname());
+    	
+    	Employee emp3=session1.get(Employee.class,3); //not found in session so query the db
+    	System.out.println(emp3);
+    	
+    	session1.close();
+    	factory.close();
+
+	}
+
+}
